69cfb0542c65e32a6f0aca2e87a5aecdfe3fb79a
BedirT <bedir.tapkan.53@gmail.com>
2016-02-03 00:32:25 -0600

    SumAdded

---
+++ sum.playground/Contents.swift
@@ -0,0 +1,12 @@
+// Print the sum of all the numbers from listOfNumbers.
+
+var listOfNumbers = [1, 2, 3, 10, 100]
+var result = 0
+for var i = 0 ; i < listOfNumbers.count ; i++ {
+    
+    result += listOfNumbers[i]
+    
+}
+
+print(result)
+
