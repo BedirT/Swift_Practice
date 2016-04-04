4ae9210cd35017eff3bd43f7f4704e6dd358890b
BedirT <bedir.tapkan.53@gmail.com>
2016-04-04 09:14:36 -0500

    Optionals Practice added

---
+++ Optionals_Practice.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+var birth = 100
+
+var gen =   birth == 1000 ? "OLEEEY" :
+            birth == 20 ? "SECOND" :
+            birth == 200 ? "THIRD" :
+            birth == 2000 ? "OKKK" :
+            "NONE"
+
+// PERFECT WAY TO USE
---
+++ projects/filter/filter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  filter
+//
+//  Created by Mustafa Bedir Tapkan on 3/26/16.
+//  Copyright © 2016 Mustafa Bedir Tapkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var entryField: UITextField!
+    @IBOutlet weak var table: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
